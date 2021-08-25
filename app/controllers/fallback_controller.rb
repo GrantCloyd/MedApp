b95e4b54967fbb7e96e4cd64ee3d7fbc09c9edbf
Grant Cloyd <dgcloyd@gmail.com>
2021-08-25 16:41:30 -0500

    reverting

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,12 +1,11 @@
 # Controller logic: fallback requests for React Router.
 # Leave this here to help deploy your app later!
 class FallbackController < ActionController
-  include ActionController::MimeResponds
+
   def index
-    # React app index page
-    # render file: 'public/index.html'
-    respond_to do |format|
-      format.html { render body: Rails.root.join('public/index.html').read }
+    React app index page
+    render file: 'public/index.html'
+
   end
 end
 end
